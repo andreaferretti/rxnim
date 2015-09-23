4618d3ff6fc40d2b6a69133b138f6cadf89072da
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-09-23 22:33:15 +0200

    Added map

--- rx.nim
+++ rx.nim
@@ -23,17 +23,30 @@ proc observer[A](xs: seq[A]): Observer[A] =
 
 proc single[A](a: A): Observer[A] = observer(@[a])
 
-proc subscriber[A](onNext: proc(a: A)): SimpleSubscriber[A] =
+proc subscriber[A](onNext: proc(a: A), onComplete: proc(), onError: proc()): SimpleSubscriber[A] =
   result.onNext = onNext
-  result.onComplete = noop
-  result.onError = noop
+  result.onComplete = onComplete
+  result.onError = onError
+
+proc subscriber[A](onNext: proc(a: A)): SimpleSubscriber[A] =
+  subscriber(onNext, noop, noop)
 
 proc subscribe[A](o: Observer[A], s: SimpleSubscriber[A]) =
   o.onSubscribe(s)
 
+proc map[A, B](o: Observer[A], f: proc(a: A): B): Observer[B] =
+  result.addOnSubscribe(proc(s: SimpleSubscriber[B]) =
+    o.subscribe(subscriber(
+      onNext = proc(a: A) = s.onNext(f(a)),
+      onComplete = s.onComplete,
+      onError = s.onError
+    ))
+  )
+
 when isMainModule:
   let
     o = observer(@[1, 2, 3, 4, 5])
+      .map(proc(x: int): auto = x * x)
     o1 = single(6)
     s = subscriber[int](println)
   o.subscribe(s)
