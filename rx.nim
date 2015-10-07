db07ae381b60dcf7b0ee8470df29c18912d29bf4
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-10-07 23:37:47 +0200

    MOved connectable in a module

--- rx.nim
+++ rx.nim
@@ -1,14 +1,9 @@
 import os, threadpool, times
-import rx/schedulers, rx/core
+import rx/schedulers, rx/core, rx/connectable
 
-export schedulers, core
+export schedulers, core, connectable
 
-type ConnectableObservable*[A] = object
-  source: Observable[A]
-  listeners: seq[Subscriber[A]]
 
-proc subscribe*[A](o: var ConnectableObservable[A], s: Subscriber[A]) =
-  o.listeners.add(s)
 
 proc println[A](a: A) = echo(a)
 
@@ -171,25 +166,6 @@ proc sendToNewThread*[A](o: Observable[A]): Observable[A] =
       onError = s.onError
     ))
   )
-
-proc publish*[A](o: Observable[A]): ConnectableObservable[A] =
-  result.listeners = @[]
-  result.source = o
-
-{.push warning[SmallLshouldNotBeUsed]: off .}
-proc connect*[A](o: ConnectableObservable[A]) =
-  o.source.subscribe(subscriber(
-    onNext = proc(a: A) =
-      for l in o.listeners:
-        l.onNext(a),
-    onComplete = proc() =
-      for l in o.listeners:
-        l.onComplete(),
-    onError = proc(e: ref Exception) =
-      for l in o.listeners:
-        l.onError(e)
-  ))
-{.pop.}
 
 when isMainModule:
   import future, sequtils
